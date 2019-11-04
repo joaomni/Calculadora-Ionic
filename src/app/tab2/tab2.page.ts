b1ce8b2a6b98021b58945dd5b22e73c821f12230
joaoqsl <joaojpmoreira25@gmail.com>
2019-11-04 15:15:59 -0300

    Page of tab1 for add values e page of tab2 for subtract values, script with to perform the calculation

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { parse } from 'path';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,18 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  private valor1: string;
+  private valor2: string;
+  private resultado: number;
+
   constructor() {}
+
+  calcula(){
+    this.resultado = parseFloat(this.valor1) - parseFloat(this.valor2);
+  }
+
+  exibeResultado(){
+    return this.resultado;
+  }
 
 }
